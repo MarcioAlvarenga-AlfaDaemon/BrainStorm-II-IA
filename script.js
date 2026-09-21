3e234738b985f28147f39508759100b4111caf16
Marcio Vinicius de Melo de Alvarenga <164529046+MarcioAlvarenga-AlfaDaemon@users.noreply.github.com>
2026-09-21 12:27:46 -0300

    s

--- script.js
+++ script.js
@@ -108,11 +108,11 @@ function mostraAlternativas(){
     }
 }
 
-function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afirmacao;
-    historiaFinal += afirmacoes + " ";
-    atual++;
-    mostraPergunta();
+function respostaSelecionada(opcaoSelecionada) {
+const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+historiaFinal += afirmacoes + "";
+atual++;
+mostraPergunta();
 }
 
 function mostraResultado(){
