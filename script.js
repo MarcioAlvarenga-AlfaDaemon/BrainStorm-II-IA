d42ecf875f291cb01f07e7ef2ea490da86575bc8
Marcio Vinicius de Melo de Alvarenga <164529046+MarcioAlvarenga-AlfaDaemon@users.noreply.github.com>
2026-09-21 12:24:02 -0300

    s

--- script.js
+++ script.js
@@ -120,5 +120,9 @@ function mostraResultado(){
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = ""; 
 }
+function aleatorio(lista) {
+const posicao = Math.floor(Math.random()* lista.length);
+return lista[posicao];
+}
 
 mostraPergunta();
